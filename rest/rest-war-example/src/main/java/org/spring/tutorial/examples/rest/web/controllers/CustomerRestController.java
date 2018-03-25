c07ee84201fe136832e049ed9d0952c5d9043148
Amdouni Mohamed Ali <amdouni.mouhamed.ali@gmail.com>
2018-03-25 23:50:35 +0200

    add rest-war-example module

---
+++ rest/rest-war-example/src/main/java/org/spring/tutorial/examples/rest/web/controllers/CustomerRestController.java
@@ -0,0 +1,78 @@
+package org.spring.tutorial.examples.rest.web.controllers;
+
+import java.util.List;
+
+import org.spring.tutorial.examples.rest.dao.CustomerDAO;
+import org.spring.tutorial.examples.rest.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerRestController {
+
+	/*
+	 * Now let us create CustomerRestController class. This class is annotated with @RestController annotation. 
+	 * Also note that we are using new annotations @GetMapping, @PostMapping, @PutMapping and @DeleteMapping instead of standard @RequestMapping.
+	 * These annotations are available since Spring MVC 4.3 and are standard way of defining REST endpoints. They act as wrapper to @RequestMapping.
+	 * For example @GetMapping is a composed annotation that acts as a shortcut for @RequestMapping(method = RequestMethod.GET).
+	 */
+	
+	@Autowired
+	private CustomerDAO customerDAO;
+
+	
+	@GetMapping("/customers")
+	public List getCustomers() {
+		return customerDAO.list();
+	}
+
+	@GetMapping("/customers/{id}")
+	public ResponseEntity getCustomer(@PathVariable("id") Long id) {
+
+		Customer customer = customerDAO.get(id);
+		if (customer == null) {
+			return new ResponseEntity("No Customer found for ID " + id, HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity(customer, HttpStatus.OK);
+	}
+
+	@PostMapping(value = "/customers")
+	public ResponseEntity createCustomer(@RequestBody Customer customer) {
+
+		customerDAO.create(customer);
+
+		return new ResponseEntity(customer, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/customers/{id}")
+	public ResponseEntity deleteCustomer(@PathVariable Long id) {
+
+		if (null == customerDAO.delete(id)) {
+			return new ResponseEntity("No Customer found for ID " + id, HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity(id, HttpStatus.OK);
+
+	}
+
+	@PutMapping("/customers/{id}")
+	public ResponseEntity updateCustomer(@PathVariable Long id, @RequestBody Customer customer) {
+
+		customer = customerDAO.update(id, customer);
+
+		if (null == customer) {
+			return new ResponseEntity("No Customer found for ID " + id, HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity(customer, HttpStatus.OK);
+	}
+}
