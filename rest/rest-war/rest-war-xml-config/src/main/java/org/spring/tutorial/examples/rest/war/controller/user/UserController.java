0f4fa67f8ad1a3a11828003c81835c14504e6a61
Amdouni Mohamed Ali <amdouni.mouhamed.ali@gmail.com>
2018-04-29 09:44:10 +0200

    add rest-war-xml-config module

---
+++ rest/rest-war/rest-war-xml-config/src/main/java/org/spring/tutorial/examples/rest/war/controller/user/UserController.java
@@ -0,0 +1,56 @@
+package org.spring.tutorial.examples.rest.war.controller.user;
+
+import org.spring.tutorial.examples.rest.war.domain.User;
+import org.spring.tutorial.examples.rest.war.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+// @RequestMapping("/users") == mapped in web xml file
+class UserController {
+
+    /*
+     * use postman to test the app
+     */
+
+    @Autowired
+    UserServiceImpl service;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<User> findAll() {
+
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)// return 200
+    public User findOne(@PathVariable("id") Long id) {
+
+        return service.findUserById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)//201
+    //the body of this answer is empty
+    public void createCustomer(@RequestBody User user /* mapping the body of the request to a User java object */) {
+
+        service.addUser(user);
+    }
+
+    @PutMapping(path = "/{id}")//for the mapping we use the property path or value its the same thing
+    @ResponseStatus(HttpStatus.NO_CONTENT)//204
+    public void update(@PathVariable("id") Long id, @RequestBody User user) {
+
+        service.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)//204
+    public void delete(@PathVariable("id") Long id) {
+
+        service.deleteUser(id);
+    }
+}
