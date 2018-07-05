ce9e63ce31e4946104342fb550ddb35a06894984
AMDOUNI Mohamed Ali <amdouni.mouhamed.ali@gmail.com>
2018-07-05 23:01:41 +0200

    [fix] refactor project dependencies structure

--- jpa/transaction-manager-example/src/test/java/org/spring/tutorial/examples/jpa/dao/UserDaoImplTest.java
+++ jpa/transaction-manager-example/src/test/java/org/spring/tutorial/examples/jpa/dao/UserDaoImplTest.java
@@ -14,38 +14,39 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
+//TODO
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes=DefaultConfig.class)
 public class UserDaoImplTest {
 
-	@Autowired
-	IUserDao userDao;
-	
-	@Before
-	@Sql(
-			scripts="/db/sql/insert-data.sql",
-			executionPhase=ExecutionPhase.BEFORE_TEST_METHOD
-	)
-	public void onInit(){
-		System.out.println("insert-data.sql executed");
-	}
+    @Autowired
+    IUserDao userDao;
 
-	//?? exceptions 
-	
-	@Test
-	public void testSelect(){
-		User user = userDao.getUserById(1);
-		System.out.println(user);
-	}
-	
-	//@Test
-	public void testSelectAll(){
-		List<User> user = userDao.findAll();
-		Assert.assertNotNull(user);
-	}
-	
-	//@Test
-	public void testDelete(){
-		userDao.deleteById(2);
-	}
+    @Before
+    @Sql(
+            scripts="/db/sql/insert-data.sql",
+            executionPhase=ExecutionPhase.BEFORE_TEST_METHOD
+    )
+    public void onInit(){
+        System.out.println("insert-data.sql executed");
+    }
+
+    //?? exceptions
+
+    @Test
+    public void testSelect(){
+        User user = userDao.getUserById(1);
+        System.out.println(user);
+    }
+
+    //@Test
+    public void testSelectAll(){
+        List<User> user = userDao.findAll();
+        Assert.assertNotNull(user);
+    }
+
+    //@Test
+    public void testDelete(){
+        userDao.deleteById(2);
+    }
 }
--- jpa/transaction-manager-example/src/test/java/org/spring/tutorial/examples/jpa/service/UserServiceImplTest.java
+++ jpa/transaction-manager-example/src/test/java/org/spring/tutorial/examples/jpa/service/UserServiceImplTest.java
@@ -1,8 +1,5 @@
 package org.spring.tutorial.examples.jpa.service;
 
-import java.util.List;
-
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.spring.tutorial.examples.jpa.DefaultConfig;
@@ -14,70 +11,68 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+
+//TODO
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes=DefaultConfig.class)
 public class UserServiceImplTest {
 
-	@Autowired
-	IUserService userService;
-	
-	@Test
-	@Sql("/db/sql/insert-data.sql")
-	public void testManipulation(){
-		try{
-			User user = new User(
-					99,
-					"Manipulated", 
-					"manipulated@manipulated.com");
-			userService.manipulateHim(user, true, false);
-		}catch(Exception e){}
-		finally{
-			selectAll();
-		}
-	}
-	
-	@Test
-	@Sql("/db/sql/insert-data.sql")
-	public void testUpdate(){
-		try{
+    @Autowired
+    IUserService userService;
 
-			User user = new User(1,"Foulen","Foulen@Foulen.com");
-			
-			//int a = userService.updateUser(user, true, false, false);
-			//rollback EXECUTED
-			
-			//int a = userService.updateUser(user, false, true, false);
-			//rollback NOT EXECUTED
-			
-			//int a = userService.updateUser(user, false, false, true);
-			//using rollback not in rollbackFor={MyDataBaseException.class} -> rollback NOT EXECUTED
-			
-		}catch(Exception e){
-		}finally{
-			selectAll();
-		}
-	}
-	
-	@Test
-	@Transactional
-	/*
-	 * by annotating the test with @Transactionnal a rollback will be raised at the end of the test so it is
-	 * not necessary to go and clean the data added by the test in the BDD
-	 */
-	public void testAnything(){}
-	
-	@Test
-	@Transactional
-	@Commit
-	/*
-	 * same as the last test but this time the data will be persisted in the database
-	 */
-	public void testAnything2(){}
-	
-	public void selectAll(){	
-		List<User> users = userService.getAll();
-		for(User user : users){
-			System.out.println(user);
-		}
-	}
+    @Test
+    @Sql("/db/sql/insert-data.sql")
+    public void testManipulation(){
+        try{
+            User user = new User(
+                    99,
+                    "Manipulated",
+                    "manipulated@manipulated.com");
+            userService.manipulateHim(user, true, false);
+        }catch(Exception e){} finally{
+            selectAll();
+        }
+    }
+
+    @Test
+    @Sql("/db/sql/insert-data.sql")
+    public void testUpdate(){
+        try{
+
+            User user = new User(1,"Foulen","Foulen@Foulen.com");
+
+            //int a = userService.updateUser(user, true, false, false);
+            //rollback EXECUTED
+
+            //int a = userService.updateUser(user, false, true, false);
+            //rollback NOT EXECUTED
+
+            //int a = userService.updateUser(user, false, false, true);
+            //using rollback not in rollbackFor={MyDataBaseException.class} -> rollback NOT EXECUTED
+
+        }catch(Exception e){
+        }finally{
+            selectAll();
+        }
+    }
+
+    @Test
+    @Transactional
+    /*
+     * by annotating the test with @Transactionnal a rollback will be raised at the end of the test so it is
+     * not necessary to go and clean the data added by the test in the BDD
+     */
+    public void testAnything(){}
+
+    @Test
+    @Transactional
+    @Commit
+    /*
+     * same as the last test but this time the data will be persisted in the database
+     */
+    public void testAnything2(){}
+
+    public void selectAll() {
+        userService.getAll().forEach(System.out::println);
+    }
 }
