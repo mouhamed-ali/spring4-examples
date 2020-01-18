4cfa434298a0b5dcc38cbe86fa8461e2e4494716
Mohamed <mamdouni@salto-consulting.com>
2020-01-18 18:16:00 +0100

    [feat] add entity manager readme file

--- jpa/entity-manager-config/src/test/java/org/spring/tutorial/examples/jpa/UserServiceImplTest.java
+++ jpa/entity-manager-config/src/test/java/org/spring/tutorial/examples/jpa/UserServiceImplTest.java
@@ -1,56 +1,60 @@
 package org.spring.tutorial.examples.jpa;
 
-import java.util.List;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.spring.tutorial.examples.jpa.domain.User;
 import org.spring.tutorial.examples.jpa.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes=DefaultConfig.class)
-@Sql(scripts="/db/sql/insert-data.sql",executionPhase=ExecutionPhase.BEFORE_TEST_METHOD)
 public class UserServiceImplTest {
 
 	@Autowired
 	IUserService userService;
-	
-	@Test
-	public void testCreate(){
-		System.out.println("--------------- test create ---------------");
-		userService.createUser(new User(99,"AAA","AAA@AAA.com"));
-		
-		System.out.println("--------------- test find ---------------");
-		User user = userService.findUserById(99);
 
-		System.out.println("--------------- test selectAll ---------------");
-		selectAll();
-		
-		System.out.println("--------------- test update ---------------");
-		user.setEmail("BBB@BBB.com");
-		user.setName("BBB");
+	@Test
+	public void testFindAll(){
+
+		int nbUsers = userService.getAll().size();
+		Assert.assertEquals(3,nbUsers);
+	}
+
+	@Test
+	public void testGetById(){
+
+		User user = userService.findUserById(1);
+		Assert.assertEquals(1,user.getId());
+		Assert.assertEquals("mkyong",user.getName());
+		Assert.assertEquals("mkyong@gmail.com",user.getEmail());
+	}
+
+	@Test
+	public void testCreateUpdateDelete(){
+
+		User user = new User(99,"pikatchu","pikatchu@gmail.com");
+		userService.createUser(user);
+
+		user = userService.findUserById(99);
+		Assert.assertNotNull(user);
+
+
+		String name = "bill";
+		String email = "bill@yahoo.com";
+		user.setName(name);
+		user.setEmail(email);
 		userService.updateUser(user);
 
-		System.out.println("--------------- test selectAll ---------------");
-		selectAll();
-		
-		System.out.println("--------------- test remove ---------------");
+		user = userService.findUserById(99);
+		Assert.assertNotNull(user);
+		Assert.assertEquals(name,user.getName());
+		Assert.assertEquals(email,user.getEmail());
+
 		userService.removeById(user);
-
-		System.out.println("--------------- test selectAll ---------------");
-		selectAll();
-		
-	}
-	
-	public void selectAll(){
-
-		List<User> users = userService.getAll();
-		for(User user : users){
-			System.out.println(user);
-		}
+		user = userService.findUserById(99);
+		Assert.assertNull(user);
 	}
 }
---
+++ jpa/readme.md
@@ -0,0 +1,13 @@
+# JPA Module
+
+In this project, we will explain how to manage the persistence layer of your application using spring.
+
+## Project structure
+
+```
+jpa
+└─── entity-manager-config
+└─── in-memory-datasource
+└─── spring-data-jpa
+└─── transaction-manager-example
+```
